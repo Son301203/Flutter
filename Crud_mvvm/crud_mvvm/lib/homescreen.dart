e28edbfdd767fb2a7ad8943c4627c9a7fba80f5f
Son301203 <121949136+Son301203@users.noreply.github.com>
2024-04-16 17:28:53 +0700

    CRUD - MVVM

---
+++ Crud_mvvm/crud_mvvm/lib/homescreen.dart
@@ -0,0 +1,247 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
+import 'dart:math';
+
+import 'package:flutter/material.dart';
+
+class SampleItem {
+  String id;
+  ValueNotifier<String> name;
+  SampleItem({String? id, required String name})
+      : id = id ?? generateUuid(),
+        name = ValueNotifier(name);
+
+  static String generateUuid() {
+    return int.parse(
+            '${DateTime.now().microsecondsSinceEpoch}${Random().nextInt(100000)}')
+        .toRadixString(35)
+        .substring(0, 9);
+  }
+}
+
+class SampleItemViewModel extends ChangeNotifier {
+  static final _instance = SampleItemViewModel._();
+  factory SampleItemViewModel() => _instance;
+  SampleItemViewModel._();
+  final List<SampleItem> items = [];
+
+  void addItem(String name) {
+    items.add(SampleItem(name: name));
+    notifyListeners();
+  }
+
+  void removeItem(String id) {
+    items.removeWhere((item) => item.id == id);
+    notifyListeners();
+  }
+
+  void updateItem(String id, String newName) {
+    try {
+      final item = items.firstWhere((item) => item.id == id);
+      item.name.value = newName;
+    } catch (e) {
+      debugPrint("Không tìm thấy mục với ID $id");
+    }
+  }
+}
+
+class SampleItemUpdate extends StatefulWidget {
+  final String? initialName;
+  const SampleItemUpdate({super.key, this.initialName});
+
+  @override
+  State<SampleItemUpdate> createState() => _SampleItemUpdateState();
+}
+
+class _SampleItemUpdateState extends State<SampleItemUpdate> {
+  late TextEditingController textEditingController;
+
+  @override
+  void initState() {
+    super.initState();
+    textEditingController = TextEditingController(text: widget.initialName);
+  }
+
+  @override
+  void dispose() {
+    textEditingController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.initialName != null ? 'Chỉnh sửa' : 'Thêm mới'),
+        actions: [
+          IconButton(
+            onPressed: () {
+              Navigator.of(context).pop(textEditingController.text);
+            },
+            icon: const Icon(Icons.save),
+          )
+        ],
+      ),
+      body: TextFormField(
+        controller: textEditingController,
+      ),
+    );
+  }
+}
+
+class SampleItemWidget extends StatelessWidget {
+  final SampleItem item;
+  final VoidCallback? onTap;
+
+  const SampleItemWidget(
+      {super.key, required this.item, this.onTap, onTaponTap});
+
+  @override
+  Widget build(BuildContext context) {
+    return ValueListenableBuilder(
+        valueListenable: item.name,
+        builder: (context, name, child) {
+          debugPrint(item.id);
+          return ListTile(
+            title: Text(name),
+            subtitle: Text(item.id),
+            leading: const CircleAvatar(
+              foregroundImage: AssetImage('assets/images/flutter_logo.png'),
+            ),
+            onTap: onTap,
+            trailing: const Icon(Icons.keyboard_arrow_right),
+          );
+        });
+  }
+}
+
+class SampleItemDetailsView extends StatefulWidget {
+  final SampleItem item;
+
+  const SampleItemDetailsView({super.key, required this.item});
+
+  @override
+  State<SampleItemDetailsView> createState() => _SampleItemDetailsViewState();
+}
+
+class _SampleItemDetailsViewState extends State<SampleItemDetailsView> {
+  final viewModel = SampleItemViewModel();
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Item Details'),
+        actions: [
+          IconButton(
+            icon: const Icon(Icons.edit),
+            onPressed: () {
+              showModalBottomSheet<String?>(
+                context: context,
+                builder: (context) =>
+                    SampleItemUpdate(initialName: widget.item.name.value),
+              ).then((value) {
+                if (value != null) {
+                  viewModel.updateItem(widget.item.id, value);
+                }
+              });
+            },
+          ),
+          IconButton(
+            icon: const Icon(Icons.delete),
+            onPressed: () {
+              showDialog(
+                  context: context,
+                  builder: (BuildContext context) {
+                    return AlertDialog(
+                      title: const Text("Xác nhận xóa"),
+                      content: const Text("Bạn có chắc chắn xóa mục này?"),
+                      actions: [
+                        TextButton(
+                          onPressed: () => Navigator.of(context).pop(false),
+                          child: const Text("Bỏ qua"),
+                        ),
+                        TextButton(
+                          onPressed: () => Navigator.of(context).pop(true),
+                          child: const Text("Xóa"),
+                        ),
+                      ],
+                    );
+                  }).then((confirmed) {
+                if (confirmed) {
+                  Navigator.of(context).pop(true);
+                }
+              });
+            },
+          ),
+        ],
+      ),
+      body: ValueListenableBuilder<String>(
+        valueListenable: widget.item.name,
+        builder: (_, name, __){
+          return Center(child: Text(name));
+        },
+      )
+    );
+  }
+}
+
+class SampleItemListView extends StatefulWidget {
+  const SampleItemListView({super.key});
+
+  @override
+  State<SampleItemListView> createState() => _SampleItemListViewState();
+}
+
+class _SampleItemListViewState extends State<SampleItemListView> {
+  final viewModel = SampleItemViewModel();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Sample Items'),
+        actions: [
+          IconButton(
+            icon: const Icon(Icons.add),
+            onPressed: (){
+              showModalBottomSheet<String?>(
+                context: context, 
+                builder: (context) => const SampleItemUpdate(),
+              ).then((value) {
+                if(value != null){
+                  viewModel.addItem(value);
+                }
+              });
+            }, 
+          )
+        ],
+      ),
+      body: ListenableBuilder(
+        listenable: viewModel,
+        builder: (context, _){
+          return ListView.builder(
+            itemCount: viewModel.items.length,
+            itemBuilder: (context, index) {
+              final item = viewModel.items[index];
+              return SampleItemWidget(
+                key: ValueKey(item.id),
+                item: item,
+                onTap: (){
+                  Navigator.of(context)
+                    .push<bool>(
+                      MaterialPageRoute(
+                        builder: (context) => SampleItemDetailsView(item: item),
+                      ),
+                    ).then((deleted) {
+                      if(deleted ?? false) {
+                        viewModel.removeItem(item.id);
+                      }
+                    });
+                },
+              );
+            },
+          );
+        },
+      )
+    );
+  }
+}
---
+++ Crud_mvvm/crud_mvvm/lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+import 'homescreen.dart';
+
+void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'CRUD MVVM',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue),
+        useMaterial3: true,
+      ),
+      home: const SampleItemListView(),
+    );
+  }
+}
+
